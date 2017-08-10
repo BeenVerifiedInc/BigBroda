4741eb29370b4aa208ed0f242277c9beda4bab45
Arian Ryan <arianryan@Arians-MacBook-Pro.local>
2017-08-10 12:52:31 -0400

    remove puts

--- lib/arel/visitors/bigquery.rb
+++ lib/arel/visitors/bigquery.rb
@@ -2,7 +2,6 @@ module Arel
   module Visitors
     class Bigquery < ToSql
       def visit_Arel_Attributes_Attribute o, collector
-        puts "Arel::Visitors::Bigquery#visit_Arel_Attributes_Attribute"
         join_name = o.relation.table_alias || o.relation.name
         collector << quote_column_name(o.name)
       end
