6237e82b20a47598817796e04a22b0d38b506f22
Arian Ryan <arianryan@Arians-MacBook-Pro.local>
2017-11-08 12:08:38 -0500

    add #visit_Arel_Nodes_Matches to bq visitor

--- lib/arel/visitors/bigquery.rb
+++ lib/arel/visitors/bigquery.rb
@@ -1,6 +1,28 @@
 module Arel
   module Visitors
     class Bigquery < ToSql
+
+      def visit_Arel_Nodes_Matches o, collector
+        collector << 'REGEXP_CONTAINS('
+        collector = visit o.left, collector
+        collector << ', '
+        collector = visit o.right, collector
+        collector << ')'
+      end
+
+      # # TODO: implement this. 
+      # def visit_Arel_Nodes_DoesNotMatch o, collector
+      #   collector = visit o.left, collector
+      #   collector << " NOT LIKE "
+      #   collector = visit o.right, collector
+      #   if o.escape
+      #     collector << ' ESCAPE '
+      #     visit o.escape, collector
+      #   else
+      #     collector
+      #   end
+      # end
+
       def visit_Arel_Attributes_Attribute o, collector
         join_name = (o.relation.table_alias || o.relation.name).split('.').last
         collector << "`#{join_name}`.#{quote_column_name(o.name)}"
